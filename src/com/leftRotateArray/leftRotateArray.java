f709bc4dddff27d87f38a7c1a59ee97e8273973d
satyam <satyamkale271@gmail.com>
2025-04-25 21:06:40 +0530

    leftRotateArray question solved

---
+++ src/com/leftRotateArray/leftRotateArray.java
@@ -0,0 +1,36 @@
+package com.leftRotateArray;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class leftRotateArray {
+
+    /*
+    Input: nums = [1, 2, 3, 4, 5]
+    Output: [2, 3, 4, 5, 1]
+     */
+
+    public static void main(String[] args) {
+        int[] num ={1, 2, 3, 4, 5};
+        int[] result = leftRotateArrayByOne(num);
+        System.out.println(Arrays.toString(result));
+
+    }
+
+
+    static int[] leftRotateArrayByOne(int[] arr){
+
+
+        int[] rotatedArray = new int[arr.length];
+
+        for(int i = 0; i < arr.length - 1; i++){
+
+            rotatedArray[i] = arr[i+1];
+
+        }
+
+        rotatedArray[arr.length -1] = arr[0];
+        return rotatedArray;
+
+    }
+}
